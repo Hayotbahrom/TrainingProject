09fd4cf30044e1da1dd3e5802325777ca665f86f
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2024-12-24 23:53:57 +0500

    AutoMapper installed and wrote mappingProfile

---
+++ src/TrainingProject.UseCase/Mappings/MappingProfile.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainingProject.Domain.Entities;
+using TrainingProject.UseCase.DTOs.Companies;
+using TrainingProject.UseCase.DTOs.Contacts;
+using TrainingProject.UseCase.DTOs.Users;
+
+namespace TrainingProject.UseCase.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //User
+            CreateMap<User, UserForCreationDto>().ReverseMap();
+            CreateMap<User, UserForUpdateDto>().ReverseMap();
+            CreateMap<User, UserForResultDto>().ReverseMap();
+
+            //Company
+            CreateMap<Company, CompanyForCreationDto>().ReverseMap();
+            CreateMap<Company, CompanyForUpdateDto>().ReverseMap();
+            CreateMap<Company, CompanyForResultDto>().ReverseMap();
+
+            //Contact
+            CreateMap<Contact, ContactForCreationDto>().ReverseMap();
+            CreateMap<Contact, ContactForUpdateDto>().ReverseMap();
+            CreateMap<Contact, ContactForResultDto>().ReverseMap();
+        }
+    }
+}
