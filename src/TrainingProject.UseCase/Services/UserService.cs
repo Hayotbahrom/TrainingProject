51f128c2791c2cbd759a7b5db69b6620cf1058ad
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2024-12-27 01:42:43 +0500

    UserService and ContactService are in progress

--- src/TrainingProject.UseCase/Services/UserService.cs
+++ src/TrainingProject.UseCase/Services/UserService.cs
@@ -1,18 +1,43 @@
-﻿using System;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrainingProject.Domain.Entities;
+using TrainingProject.Domain.Interfaces.Repositories;
 using TrainingProject.UseCase.Contracts;
 using TrainingProject.UseCase.DTOs.Users;
+using TrainingProject.UseCase.Exceptions;
 
 namespace TrainingProject.UseCase.Services
 {
     public class UserService : IUserService
     {
-        public Task<UserForResultDto> AddAsync(UserForCreationDto dto)
+        private readonly IUserRepository userRepository;
+        private readonly IMapper mapper;
+        public UserService(IUserRepository userRepository, IMapper mapper)
         {
-            throw new NotImplementedException();
+            this.userRepository = userRepository;
+            this.mapper = mapper;
+        }
+        public async Task<UserForResultDto> AddAsync(UserForCreationDto dto)
+        {
+            var user = await userRepository.SelectAll()
+                .Where(x => x.Username == dto.Username)
+                .FirstOrDefaultAsync();
+
+            if (user is not null)
+            {
+                throw new ProjectException(403, "User is already exist");
+            }
+            
+            var mappedUser = mapper.Map<User>(dto);
+
+            var result = await userRepository.CreateAsync(mappedUser);
+
+            return mapper.Map<UserForResultDto>(result);    
         }
 
         public Task<IEnumerable<UserForResultDto>> GetAllAsync()
@@ -25,7 +50,7 @@ namespace TrainingProject.UseCase.Services
             throw new NotImplementedException();
         }
 
-        public Task<UserForResultDto> ModifyAsync(UserForUpdateDto dto)
+        public Task<UserForResultDto> ModifyAsync(Guid id, UserForUpdateDto dto)
         {
             throw new NotImplementedException();
         }
