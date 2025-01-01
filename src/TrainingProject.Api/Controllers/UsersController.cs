c06b19b133bdfd04c2152c451a5cd48b4af8e34f
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2025-01-02 00:50:55 +0500

    UsersController added

---
+++ src/TrainingProject.Api/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrainingProject.UseCase.Contracts;
+using TrainingProject.UseCase.DTOs.Users;
+
+namespace TrainingProject.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService userService;
+        public UsersController(IUserService userService) 
+        {
+            this.userService = userService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+            => Ok( await userService.GetAllAsync());
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync([FromRoute(Name = "id")] Guid id)
+            => Ok(await userService.GetByIdAsync(id));
+        
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync([FromRoute(Name = "id")] Guid id)
+            =>Ok( await userService.RemoveAsync(id));
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> ModifyAsync([FromRoute(Name = "id")] Guid id, [FromForm]UserForUpdateDto dto)
+            => Ok( await userService.ModifyAsync(id, dto));
+
+        [HttpPost]
+        public async Task<IActionResult> AddAsync([FromForm] UserForCreationDto dto)
+            => Ok(await userService.AddAsync(dto));
+    }
+}
