867fe19a1b37ce0278b5239fae117a4fa09742e9
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2025-01-11 12:03:45 +0500

    Created Shared Layer

---
+++ TrainingProject.Shared/DTOs/Contacts/ContactForCreationDto.cs
@@ -0,0 +1,12 @@
+﻿namespace TrainingProject.Shared.DTOs.Contacts;
+
+public class ContactForCreationDto
+{
+    public Guid CompanyId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Position { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string Notes { get; set; }
+}
