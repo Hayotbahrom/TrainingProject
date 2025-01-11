62180da4dcdad2394fe1843dbd1ad662ab7ff4ae
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2025-01-11 10:25:17 +0500

    CompanyForm service in progress

---
+++ TrainingProject.Proxy/Services/CompanyFormService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingProject.Proxy.Services
+{
+    public class CompanyFormService
+    {
+        private readonly HttpClient httpClient;
+        public CompanyFormService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public Task AddAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
