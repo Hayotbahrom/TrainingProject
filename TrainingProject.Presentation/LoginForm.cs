1dfcbf6eb14a82e64d93f086534346e572d1d41c
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2025-01-11 10:23:41 +0500

    conflict in presentation layer. Created new forms

--- TrainingProject.Presentation/LoginForm.cs
+++ TrainingProject.Presentation/LoginForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TrainingProject.Presentation
 {
-    public partial class MainForm : Form
+    public partial class LoginForm : Form
     {
-        public MainForm()
+        public LoginForm()
         {
             InitializeComponent();
         }
