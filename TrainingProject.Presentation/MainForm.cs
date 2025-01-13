20d2d0b4e2882a0578d565b597ded494fb50b2ee
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2025-01-13 11:04:23 +0500

    UI changed

--- TrainingProject.Presentation/MainForm.cs
+++ TrainingProject.Presentation/MainForm.cs
@@ -16,5 +16,10 @@ namespace TrainingProject.Presentation
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
