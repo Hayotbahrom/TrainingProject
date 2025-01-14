c03eb6b20bc1ee0752e09ca3105803856ce5bf84
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2025-01-14 12:54:29 +0500

    ContactForm service getall implemented

---
+++ TrainingProject.Proxy/Services/ContactFormService.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Text;
+using TrainingProject.Shared.DTOs.Contacts;
+
+namespace TrainingProject.Proxy.Services;
+
+public class ContactFormService
+{
+    private readonly HttpClient httpClient;
+    private string baseUrl = "https://localhost:7143/api/";
+    public ContactFormService(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+    public async Task<bool> AddAsync(ContactForCreationDto model)
+    {
+        string url = baseUrl + "Contacts";
+        var jsonContent = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json");
+        var response = await httpClient.PostAsync(url, jsonContent);
+
+        if (response.IsSuccessStatusCode)
+            return true;
+        return false;
+    }
+    public async Task<IEnumerable<ContactForResultDto>> GetAllAsync()
+    {
+        string url = baseUrl + "Contacts";
+        var response = await httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string responseData = await response.Content.ReadAsStringAsync();
+
+            // Add logging to see the raw response
+            Console.WriteLine($"Response Data: {responseData}");
+
+            var result = JsonSerializer.Deserialize<IEnumerable<ContactForResultDto>>(responseData, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            return result;
+        }
+        else
+        {
+            throw new HttpRequestException($"Failed to fetch data: {response.StatusCode} - {response.ReasonPhrase}");
+        }
+    }
+}
+
