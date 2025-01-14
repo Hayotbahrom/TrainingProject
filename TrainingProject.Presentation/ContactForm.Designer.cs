f0445bb8100382b8ab50f728c4b9839dfe33e4b3
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2025-01-14 08:15:10 +0500

    ContactFrom started

---
+++ TrainingProject.Presentation/ContactForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace TrainingProject.Presentation
+{
+    partial class ContactForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnLogout = new Button();
+            SuspendLayout();
+            // 
+            // btnLogout
+            // 
+            btnLogout.BackColor = Color.IndianRed;
+            btnLogout.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLogout.ForeColor = SystemColors.ButtonFace;
+            btnLogout.Location = new Point(629, 32);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(108, 51);
+            btnLogout.TabIndex = 4;
+            btnLogout.Text = "Log out";
+            btnLogout.UseVisualStyleBackColor = false;
+            btnLogout.Click += btnLogout_Click;
+            // 
+            // ContactForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnLogout);
+            Name = "ContactForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ContactForm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnLogout;
+    }
+}
---
+++ TrainingProject.Presentation/ContactForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrainingProject.Presentation
+{
+    public partial class ContactForm : Form
+    {
+        public ContactForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            LoginForm loginForm = new LoginForm();
+            loginForm.ShowDialog();
+        }
+    }
+}
