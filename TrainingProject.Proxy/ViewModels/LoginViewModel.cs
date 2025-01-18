5113010829bd9c0994cb4a8432c0b5d7dcfcd125
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2025-01-18 09:35:06 +0500

    Mvvm: Login method done

--- TrainingProject.Proxy/ViewModels/LoginViewModel.cs
+++ TrainingProject.Proxy/ViewModels/LoginViewModel.cs
@@ -1,14 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrainingProject.Proxy.Services;
+using TrainingProject.Shared.DTOs.Users;
 
 namespace TrainingProject.Proxy.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : INotifyPropertyChanged 
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        private string _username;
+        private string _password;
+        private string _loginMessage;
+        private readonly LoginFormService _formService;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public LoginViewModel() 
+        {
+            _formService = new LoginFormService(new HttpClient());
+        }
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string LoginMessage
+        {
+            get => _loginMessage;
+            set
+            {
+                _loginMessage = value;
+                OnPropertyChanged(nameof(LoginMessage));
+            }
+        }
+        public async Task<bool> LoginAsync()
+        {
+            var loginModel = new UserForCreationDto
+            {
+                Username = Username,
+                Password = Password,
+            };
+            try
+            {
+                var tokenResponse = await _formService.LoginAsync(loginModel);
+                if (tokenResponse != null)
+                {
+                    LoginMessage = "Login Successfully.";
+                    return true;
+                }
+                else
+                {
+                    LoginMessage = "Login failed. No valid token was returned.";
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                LoginMessage = $"Login failed. Error: {ex.Message}";
+                return false;
+            }
+        }
     }
 }
